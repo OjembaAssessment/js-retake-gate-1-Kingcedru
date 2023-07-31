a5d99f6b82a38036e82e037f909a264c5395b1e2
cedrick byishimo <byishimocedrick@gmail.com>
2023-07-31 10:25:33 +0200

    first commit

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -13,8 +13,14 @@ for(let x = 0; x < parTextLower.length; x++){
         parStore[parTextLower[x]] = 1
     }
 }
-console.log(parStore)
+let textSort = Object.fromEntries(Object.entries(parStore).sort(([,a], [,b]) => b - a))
+let appier12 = Object.keys(Object.fromEntries(Object.entries(textSort).slice(1,13)))
+
 // let sortedPara = Object.fromEntries(Object.entries(paraObject).sort(([,a], [,b]) => b - a))
 // let twelveCommon = Object.keys(Object.fromEntries(Object.entries(sortedPara).slice(1,13)))
+let one = document.createElement("p")
+one.setAttribute("class", "one")
+one.textContent = twelveCommon[0]
+cloud.append(one)
 
 console.log("Exercise 2 - Word Cloud");
